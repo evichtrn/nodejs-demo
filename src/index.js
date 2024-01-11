727f257f2021b4365f16eb77f750b08061d856e1
Nhật Trần <huynhat.fe@outlook.com.vn>
2024-01-11 17:55:25 +0700

    setup no.8

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Template engine
 app.engine(
-  "hbs",
+  'hbs',
   engine({
-    extname: ".hbs",
-  })
+    extname: '.hbs',
+  }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Home, search, contact
 
@@ -29,4 +29,4 @@ route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});
+               });
