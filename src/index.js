3998e0be696113a9738867a640a6dd60770c8cd0
Nhật Trần <huynhat.fe@outlook.com.vn>
2024-01-11 09:57:53 +0700

    setup no.6

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP logger
@@ -20,25 +22,10 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// Home, search, contact
 
-app.get("/courses", (req, res) => {
-  res.render("courses");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  res.render("search");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
